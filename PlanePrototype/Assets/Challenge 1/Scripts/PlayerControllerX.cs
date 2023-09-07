9f7c7c5ddcf333a2e87ef928d0a7aa6d5cf7f14f
ChangxiongLiu <77821263+ChangxiongLiu@users.noreply.github.com>
2023-09-07 15:39:49 -0700

    initial commit

---
+++ PlanePrototype/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    public float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update at a fixed time
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed);
+
+        // tilt the plane
+        if (Mathf.Abs(verticalInput) > 0.1f)
+        {
+            transform.Rotate(Vector3.left * verticalInput * rotationSpeed * Time.deltaTime);
+        }
+        
+    }
+}
